79bb22b9814fc9e537bde5995c320f4f35c4ac1d
adwaithwas <betteradwaith@gmail.com>
2025-01-11 11:46:24 +0530

    added: error handling, tracking of wrong answers and displays them at the end

--- questions.py
+++ questions.py
@@ -2,63 +2,63 @@ class Questions:
     def __init__(self):
         self.quiz = {
             'question1' : {
-                'question' : 'What is the capital of France?',
+                'question' : 'Ques: What is the capital of France?',
                 'options' : 'a.Paris\tb.Dubai\tc.New Delhi\td.Tokyo',
                 'answer' : 'a'
             },
 
             'question2' : {
-                'question' : 'what is the capital of Germany',
+                'question' : 'Ques: What is the capital of Germany',
                 'options' : 'a.Moscow\tb.Berlin\tc.Osaka\td.Indore',
                 'answer' : 'b'
             },
             
             'question3': {
-                'question': 'What is the largest planet in our solar system?',
+                'question': 'Ques: What is the largest planet in our solar system?',
                 'options': 'a. Earth  b. Mars  c. Jupiter  d. Venus',
                 'answer': 'c'
             },
 
             'question4': {
-                'question': 'Which element has the chemical symbol "O"?',
+                'question': 'Ques: Which element has the chemical symbol "O"?',
                 'options': 'a. Oxygen  b. Osmium  c. Ozone  d. Olivine',
                 'answer': 'a'
             },
 
             'question5': {
-                'question': 'Who wrote the play "Romeo and Juliet"?',
+                'question': 'Ques: Who wrote the play "Romeo and Juliet"?',
                 'options': 'a. William Shakespeare  b. Charles Dickens  c. Jane Austen  d. Mark Twain',
                 'answer': 'a'
             },
 
             'question6': {
-                'question': 'What is the hardest natural substance on Earth?',
+                'question': 'Ques: What is the hardest natural substance on Earth?',
                 'options': 'a. Gold  b. Diamond  c. Iron  d. Graphite',
                 'answer': 'b'
             },
 
             'question7': {
-                'question': 'What is the smallest country in the world?',
+                'question': 'Ques: What is the smallest country in the world?',
                 'options': 'a. Monaco  b. Vatican City  c. San Marino  d. Liechtenstein',
                 'answer': 'b'
             },
 
             'question8': {
-                'question': 'Who painted the Mona Lisa?',
+                'question': 'Ques: Who painted the Mona Lisa?',
                 'options': 'a. Vincent van Gogh  b. Pablo Picasso  c. Leonardo da Vinci  d. Claude Monet',
                 'answer': 'c'
             },
 
             'question9': {
-                'question': 'What is the square root of 64?',
+                'question': 'Ques: What is the square root of 64?',
                 'options': 'a. 6  b. 7  c. 8  d. 9',
                 'answer': 'c'
             },
 
             'question10': {
-                'question': 'Which animal is known as the "King of the Jungle"?',
+                'question': 'Ques: Which animal is known as the "King of the Jungle"?',
                 'options': 'a. Elephant  b. Tiger  c. Lion  d. Gorilla',
                 'answer': 'c'
             }
-            
+
         }
--- quiz.py
+++ quiz.py
@@ -5,6 +5,7 @@ class Quiz():
     def __init__(self):
         self.quiz = ques.Questions().quiz
         self.score = 0
+        self.wrong_ans = []
 
     def start_quiz(self):
 
@@ -17,12 +18,43 @@ class Quiz():
             print(self.quiz[i]['options'])
 
             #input the answer
-            ans = input('please enter the correct option(a,b,c,d): ').lower()
-            if ans == self.quiz[i]['answer']: self.score += 1
+            while(True):
+                try:
+                    ans = input('please enter the correct option(a,b,c,d): ').lower()
+                    if ans not in ('a','b','c','d'):
+                        raise ValueError("incorrect value")
+                    else:
+                        if ans == self.quiz[i]['answer']:
+                            self.score += 1
+                        else: 
+                            self.wrong_ans.append(i)
+                        print("===#===#===#===")
+                        break
+                except ValueError as e:
+                    print("===#===#===#===")
+                    print(f"error: {e}")
+                    print(f"please enter a legal answer: (a,b,c,d)")
+                    print("===#===#===#===")
+                    print(self.quiz[i]['question'])
+                    print(self.quiz[i]['options'])
 
         print("===#===#===#===")
+        print("TEST FINISHED")
+        print("===#===#===#===")
+
+        if self.score != len(self.quiz):
+            print(f"the questions you attempted wrong are:".upper())
+            for i in self.wrong_ans:
+                print(self.quiz[i]['question'])
+                print(self.quiz[i]['options'])
+                print("correct ans: " + self.quiz[i]['answer'])
+                print("===#===#===#===")
+
         print(f"your score is: {self.score}/{len(self.quiz)}")
-            
+        print("===#===#===#===")
+
+        
+
 
 game = Quiz()
 game.start_quiz()
